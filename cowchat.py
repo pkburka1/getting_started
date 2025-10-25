110edd7ca8c01db47832fa960fbf4b8ccf909f05
pkburka1 <pkburka1@sheffield.ac.uk>
2025-10-25 11:25:42 +0530

    Add cowchat.py and uv.lock files

---
+++ cowchat.py
@@ -0,0 +1,18 @@
+
+import cowsay
+
+cowsay.cow("Hello! Moo! Moo!")
+import cowsay
+
+cowsay.cow("Hello! Moo! Moo!")
+
+while True:
+    input_text = input("Chat, or type 'quit': ")
+    if input_text.lower() == "quit":
+        cowsay.cow("Goodbye! Moo! Moo! Moo!")
+        break
+
+    cowsay.cow(input_text + " Moo!")
+    chat
+    'chat'
+    chat
